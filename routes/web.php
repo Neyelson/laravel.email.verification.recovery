6bec8e2c6251d7913dc2a6fc8bf95848329392b2
Neyelson Alves <neyelson@gmail.com>
2023-11-06 00:40:35 -0300

    implementando envio de emails 2

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Auth::routes([
+    'verify' => true
+]);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
